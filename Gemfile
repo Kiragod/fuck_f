bd4ae957f47f89bd9c8013a986c91679ef31c0a6
kiragod <Kiragods@gmail.com>
2011-11-06 15:11:27 +0200

    first

---
+++ Gemfile
@@ -0,0 +1,23 @@
+source 'http://rubygems.org'
+
+gem 'rails'
+gem 'sqlite3'
+
+
+group :assets do
+  gem 'sass-rails', "  ~> 3.1.0"
+  gem 'coffee-rails', "~> 3.1.0"
+  gem 'uglifier'
+end
+
+group :development do
+  gem 'rspec-rails', '2.6.1'
+end
+
+group :test do
+  gem 'rspec-rails', '2.6.1'
+  gem 'webrat', '0.7.1'
+end
+
+
+gem 'jquery-rails'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FuckF::Application.config.secret_token = '40fef3629c9b8c165dcdf5e9a5754f3fc435ee1e9361db585e326fedc9dc0772cf62a9a8aad1d04e12a706b341821b0eae21d9d013a8e2fabba675123f924221'
